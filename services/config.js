31678f050f41f40cf62c0ed6fdeebe490787b75f
D.Ghazvinie <68420629+Ghazvinie@users.noreply.github.com>
2021-04-21 22:24:50 +0100

    Update config.js

--- services/config.js
+++ services/config.js
@@ -5,9 +5,9 @@ configRepo.SetConfig = (paypal) => {
     host: "api.sandbox.paypal.com",
     port: "",
     client_id:
-      "AYWoGsnL-hOI6dDQYTz2cS3dvEWJT9yhvMBadZPH5KnmEf9Dm_JuBwHqPEim-6QQEwcEbc2xI3YrTOuR",
+      "",
     client_secret:
-      "EOBX0FQAXRttMDgILQcQSc0Y3NbJeVP9NfURYgU4StWTdR7-LUdPD1WBsHWt8AlXoV32V6v3B3WS--1w",
+      "",
   };
 
   paypal.configure(config);
